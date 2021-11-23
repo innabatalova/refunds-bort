a46cebee406730acb6c26b05a8da2bb5a69044ac
innabatalova <batalovainna97@gmail.com>
2021-11-23 15:12:15 +0300

    Create: added mack part 1

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  //маска для проверки ввода номера
+
+  // $(".data-user__input__field").mask("+7 (900) 000 00 00", (minlength = 10));+
+  //задаете x как любое число
+  // $.mask.definitions["x"] = "[0-9]";
+
+  // //устанавливаете саму маску и * вместо подчеркивания
+  // $("id").mask("4276-xxxx-xx98-7654", { placeholder: "*" });
+
+  // $.mask.definitions["x"] = "[0-9]";
+
+  //плагин фиксации курсора в начале поля
+  $.fn.setCursorPosition = function (pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+  };
+
+  //маска ввода серии паспорта
+  $("#series")
+    .click(function () {
+      $(this).setCursorPosition(0);
+    })
+    .mask("99 99", { autoclear: false, placeholder: "*" });
+
+  //маска ввода номера паспорта
+  $("#number")
+    .click(function () {
+      $(this).setCursorPosition(0);
+    })
+    .mask("99 99 99", { autoclear: false, placeholder: "*" });
+
+  //маска ввода БИК
+  $("#BIK")
+    .click(function () {
+      $(this).setCursorPosition(0);
+    })
+    .mask("999999999", { autoclear: false, placeholder: "*" });
+
+  //маска ввода корр. счета
+  $("#corr")
+    .click(function () {
+      $(this).setCursorPosition(3);
+    })
+    .mask("30199999999999999999", { autoclear: false, placeholder: "*" });
+});
